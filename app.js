d5536b6bfae7594bbaf359cc207fb9998b28e5d8
anjelrasong <anjelrasong@samsungfiresvc.com>
2020-08-06 20:22:25 +0900

    포폴 작업중

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+console.log(path.join(__dirname, './public/index.html'));
+app.use(express.static(path.join(__dirname, './public')));
+// app.use(express.static(path.join(__dirname, './public/index.html')));
+
+app.listen(3005, function() {
+    console.log('3005');
+})
+
+
+module.exports = app;
